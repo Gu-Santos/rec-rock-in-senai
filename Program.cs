b01884439d879c3e9096a7d2aef4c5de16839c63
NatanPereiraFurtado <natan.p.furtado@gmail.com>
2021-07-19 17:11:29 -0300

    att com funcao

--- Program.cs
+++ Program.cs
@@ -5,13 +5,36 @@ namespace rock_in_senai_tarde
 {
     class Program
     {
+            static List<string> inscritos = new List<string>();
         static void Main(string[] args)
         {
-            string resposta_responsavel;
-            bool cadastros = true;
 
-            List<string> inscritos = new List<string>();
+            bool menu = true;
+            do
+            {
             
+            Console.WriteLine("\nDigite 1 para se inscrever, ou digite 2 para mostrar a lista de inscritos");
+            string resposta_menu = Console.ReadLine();
+            
+            if (resposta_menu == "1")
+            {
+                sistema_cadastro();
+            }
+            else
+            {
+                mostrar_lista();
+            }
+
+            } while (menu);
+        }
+
+
+        static bool sistema_cadastro()
+        {
+            string resposta_responsavel;
+            bool cadastro = true;
+            bool cadastro_feito = true;
+
             do
             {
                 Console.WriteLine("Qual o seu nome?");
@@ -24,6 +47,7 @@ namespace rock_in_senai_tarde
                 {
                     Console.WriteLine("Parabéns! Sua inscrição foi feita!\n");
                     inscritos.Add(nome);
+                    cadastro_feito = true;
                 }
                 else
                 {
@@ -34,24 +58,34 @@ namespace rock_in_senai_tarde
                     {
                         Console.WriteLine("Então você pode ir com o seu resposável!\n");
                         inscritos.Add(nome);
+                        cadastro_feito = true;
                     }
                     else
                     {
                         Console.WriteLine("Você não tem a idade para ir ao evento sozinho, e não pode ir sem um resposável!\n");
+                        cadastro_feito = false;
                     }
                 }
 
                 Console.WriteLine("Deseja cadastrar mais uma pessoa?s/n");
                 string continuar_cadastro = Console.ReadLine();
+
                 if (continuar_cadastro == "n")
                 {
-                    cadastros = false;
-                }else
+                    cadastro = false;
+                }
+                else
                 {
-                    cadastros = true;
+                    cadastro = true;
                 }
 
-            } while (cadastros);
+            } while (cadastro);
+
+            return cadastro_feito;
+        }
+
+        static void mostrar_lista()
+        {
 
             foreach (var item in inscritos)
             {
